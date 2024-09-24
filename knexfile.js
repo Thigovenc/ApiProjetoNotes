0b0e12f17e8e238eb4cea6c233eb06b1eef06bfa
thigovenc <thiagovenceslau1997@gmail.com>
2024-09-24 13:54:54 -0300

    MIgrations for link and tag

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname,"src","database","database.db")
     },
+    pool: {
+      afterCreate: (conn,cb) => coon.run("PRAGMA foreign_keys = ON", cb)
+    },
     migrations: {
       directory: path.resolve(__dirname,"src","database","knex","migrations")
     },
