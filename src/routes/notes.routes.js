66398d68bf0ae7e2baeb5ceb29060d6f38e60658
thigovenc <thiagovenceslau1997@gmail.com>
2024-09-25 10:27:14 -0300

    registering notes

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,15 @@
+const {Router} = require("express");
+
+const NotesController = require("../controllers/NotesController")
+
+
+
+const notesRoutes = Router();
+
+const notesController = new NotesController();
+
+notesRoutes.post("/:user_id",notesController.create);
+
+
+module.exports = notesRoutes; //Exportando para qualquer um utilizar.
+
