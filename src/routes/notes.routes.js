d0dc826043498b4e43b68a9f60723951b817f911
thigovenc <thiagovenceslau1997@gmail.com>
2024-09-25 11:38:58 -0300

    show and delete notes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -9,7 +9,8 @@ const notesRoutes = Router();
 const notesController = new NotesController();
 
 notesRoutes.post("/:user_id",notesController.create);
-
+notesRoutes.get("/:id",notesController.show);
+notesRoutes.delete("/:id",notesController.delete);
 
 module.exports = notesRoutes; //Exportando para qualquer um utilizar.
 
