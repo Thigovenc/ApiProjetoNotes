3cb234e2d9dbb58e4ed0cdd06504db74b91eb30d
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-11 13:18:28 -0300

    feat: adds generation token user

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
 const {compare} = require("bcryptjs")
+const authConfig = require("../configs/auth")
+const {sign} = require("jsonwebtoken")
 class SessionsController{
     async create(request, response){
         const {email, password} = request.body; 
@@ -12,9 +14,13 @@ class SessionsController{
             throw new AppError("E-mail e/ou senha incorreta", 401)
         }
 
-
+        const {secret,expiresIn } = authConfig.jwt;
+        const token = sign({},secret,{
+            subject: String(user.id),
+            expiresIn
+        })
         
-        return response.json(user);
+        return response.json({user,token});
     }
 }
 
