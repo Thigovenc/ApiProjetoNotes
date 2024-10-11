51d5f1018b091db0c4b2745c40685a30465d8977
thigovenc <thiagovenceslau1997@gmail.com>
2024-10-11 12:52:25 -0300

    feat: adds validate password

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,15 +1,19 @@
 const knex = require("../database/knex")
 const AppError = require("../utils/AppError")
-
+const {compare} = require("bcryptjs")
 class SessionsController{
     async create(request, response){
         const {email, password} = request.body; 
         
         const user = await knex("users").where({email}).first()
+        const passwordMatched = await compare(password,user.password);
 
-        if(!user){
+        if(!user | !passwordMatched){
             throw new AppError("E-mail e/ou senha incorreta", 401)
         }
+
+
+        
         return response.json(user);
     }
 }
